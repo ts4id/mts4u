9772b42905a182a844524de3f84ae7e969d67d55
Mod The Sims 4U <32659578+ts4id@users.noreply.github.com>
2018-03-24 17:42:15 +0700

    Update mts4u-redirect.js

--- mts4u-redirect.js
+++ mts4u-redirect.js
@@ -26,4 +26,3 @@ if(a_to_vk)
 function a_to_fa()
 {var a_to_vf=new Array();protected_links=protected_links.replace(" ","");a_to_vf=protected_links.split(",");return a_to_vf;}
 protected_links = "modthesims4u.blogspot.com";
-auto_safelink();
